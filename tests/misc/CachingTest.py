8286c80f7446b488d13cf28526c2530ef27ae9e5
Palash Nigam <npalash25@gmail.com>
2018-03-15 16:54:46 +0530

    config: replace [default] with [all]
    Replaces all instances of [default] with [all]
    
    Fixes: https://github.com/coala/coala/issues/4127

--- tests/misc/CachingTest.py
+++ tests/misc/CachingTest.py
@@ -159,9 +159,6 @@ class CachingTest(unittest.TestCase):
                    '-c', filename + '.coafile',
                    '-f', filename + 'test.py')
                 self.assertIn('This file has', stdout)
-                self.assertIn(
-                    'Implicit \'Default\' section inheritance is deprecated',
-                    stderr)
 
             retval, stdout, stderr = execute_coala(
                coala.main,
@@ -170,10 +167,7 @@ class CachingTest(unittest.TestCase):
                '-c', filename + '.coafile',
                '-f', filename + 'test.py')
             self.assertIn('This file has', stdout)
-            self.assertEqual(2, len(stderr.splitlines()))
+            self.assertEqual(1, len(stderr.splitlines()))
             self.assertIn(
                 'LineCountTestBear: This result has no patch attached.',
-                stderr)
-            self.assertIn(
-                'Implicit \'Default\' section inheritance is deprecated',
                 stderr)
